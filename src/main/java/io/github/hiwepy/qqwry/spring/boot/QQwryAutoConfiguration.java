3909bc4699a27eb5d0185f8e1f82963d9b678951
wandl-6A72h <hiwepy@gmail.com>
2026-05-16 14:12:29 +0800

    refactor: migrate groupId and packages from com.github.hiwepy to io.github.hiwepy

---
+++ src/main/java/io/github/hiwepy/qqwry/spring/boot/QQwryAutoConfiguration.java
@@ -0,0 +1,57 @@
+package io.github.hiwepy.qqwry.spring.boot;
+
+import io.github.hiwepy.qqwry.spring.boot.ext.QQWry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ *
+ */
+@Configuration
+@EnableConfigurationProperties({ QQwryProperties.class })
+public class QQwryAutoConfiguration {
+
+	protected ResourceLoader resourceLoader = new PathMatchingResourcePatternResolver();
+
+	@Autowired
+	private QQwryProperties properties;
+
+	@Bean
+	public QQWry qqwry() throws IOException {
+		QQWry qqwry = null;
+		if(properties.isExternal()) {
+			try {
+				if(new File(properties.getLocation()).exists()) {
+					qqwry = new QQWry(Paths.get(properties.getLocation())); // load qqwry.dat from java.nio.file.Path
+				} else {
+					// 查找resource
+					Resource resource = resourceLoader.getResource(properties.getLocation());
+					if(resource.exists()) {
+						ByteArrayOutputStream output = new ByteArrayOutputStream();
+						FileCopyUtils.copy(resource.getInputStream(), output);
+						qqwry = new QQWry(output.toByteArray()); // create QQWry with provided data
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				qqwry = new QQWry(); // load qqwry.dat from classpath
+			}
+		} else {
+			qqwry = new QQWry(); // load qqwry.dat from classpath
+		}
+
+		return qqwry;
+	}
+
+}
---
+++ src/test/java/io/github/hiwepy/qqwry/spring/boot/Application.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.hiwepy.qqwry.spring.boot;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.hiwepy.qqwry.spring.boot.ext.QQWry;
+
+@SpringBootApplication
+public class Application {
+
+	@Autowired
+	QQWry qqwry;
+
+	@PostConstruct
+	public void test() throws IOException {
+
+		long startTime = System.currentTimeMillis();
+		System.out.println(qqwry.findIP("61.94.43.82"));
+		System.out.println(qqwry.findIP("127.0.0.1"));
+		System.out.println("Time :" + (System.currentTimeMillis() - startTime));
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
