1dee80b889af95adf5df8332ca207fa32f868984
wandl <hiwepy@163.com>
2026-05-27 10:51:50 +0800

    chore(pom): align to Spring Boot 3.3.x (JDK 17, Boot 3.3.13)

--- src/test/java/com/github/hiwepy/qqwry/spring/boot/Application.java
+++ src/test/java/com/github/hiwepy/qqwry/spring/boot/Application.java
@@ -17,7 +17,7 @@ package com.github.hiwepy.qqwry.spring.boot;
 
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
